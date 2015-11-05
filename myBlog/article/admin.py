11674b08ad26a85d3494f415f0da1f9b880bbc92
Ruter <ruter.lu@gmail.com>
2015-11-05 20:44:56 +0800

    Setting admin site

--- myBlog/article/admin.py
+++ myBlog/article/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from article.models import *
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'date')    #Setting display fields
+    search_fields = ['title']                       #Setting search fields
+    list_filter = ('date',)                         #Setting filter by post date
+    filter_horizontal = ('tag',)                    #Horizontal display tag's select field
+
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Category)
+admin.site.register(Tag)
+
