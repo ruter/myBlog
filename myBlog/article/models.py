bda7266fa91849c1ad5b78dcd46d981737d3d4cc
Ruter <ruter.lu@gmail.com>
2015-11-05 20:45:44 +0800

    Add 'tag' field

--- myBlog/article/models.py
+++ myBlog/article/models.py
@@ -6,12 +6,26 @@ class Category(models.Model):
     def __unicode__(self):
         return self.name
 
+    class Meta:
+        ordering = ['name']
+
+
+class Tag(models.Model):
+    tag = models.CharField(u'Tag', max_length=20)                   #Tag
+
+    def __unicode__(self):
+        return self.tag
+
+    class Meta:
+        ordering = ['tag']
+
 
 class Article(models.Model):
     title = models.CharField(u'Title', max_length=50)               #Article's title
     content = models.TextField(u'Content')                          #Article's content
-    date = models.DateField(u'Date', auto_now_add=True)             #Article's post date
+    tag = models.ManyToManyField(Tag, blank=True)                   #Article's tags
     category = models.ForeignKey('Category', blank=True, null=True) #Article's category
+    date = models.DateField(u'Date', auto_now_add=True)             #Article's post date
 
     def __unicode__(self):
         return self.title
