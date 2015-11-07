206286d81c313034d9d8f32a522b91bfd91a4d8e
Ruter <ruter.lu@gmail.com>
2015-11-07 10:37:58 +0800

    Add views

--- myBlog/article/views.py
+++ myBlog/article/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from article.models import *
 
 def home(request):
-    return HttpResponse("Hello world!")
+    info = BlogInfo.objects.get(pk = 1)
+    return render_to_response('index.html', {'info': info})
+
+def about(request):
+    return render_to_response('about.html')
+
+def contact(request):
+    return render_to_response('contact.html')
+
+def blog(request, pk):
+    blog = Article.objects.get(pk = int(pk))
+    return render_to_response('single.html', {'blog': blog})
