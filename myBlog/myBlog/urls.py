47880c158ca9e539569d337b7ff1eb7384930bc2
Ruter <ruter.lu@gmail.com>
2015-11-08 16:50:19 +0800

    Add tag & category url patterns

--- myBlog/myBlog/urls.py
+++ myBlog/myBlog/urls.py
@@ -19,10 +19,14 @@ from django.contrib import admin
 from article import views as article
 
 urlpatterns = [
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
     url(r'^$', article.home, name='home'),
     url(r'^index\.html$', article.home, name='index'),
     url(r'^about\.html$', article.about, name='about'),
     url(r'^contact\.html$', article.contact, name='contact'),
     url(r'^blog/article=(?P<pk>\d+)/$', article.blog, name='blog'),
+    url(r'^tag/(?P<tag>\w+)/$', article.tag, name='tag'),
+    url(r'^category/(?P<cate>\w+)/$', article.category, name='category'),
 ]
