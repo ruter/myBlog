210d4c5de9b7d16727c6d741c712be50add2d0f5
Ruter <ruter.lu@gmail.com>
2015-11-08 10:47:30 +0800

    Fix a stupid bug

--- myBlog/article/views.py
+++ myBlog/article/views.py
@@ -1,19 +1,33 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 from article.models import *
 from django import forms
+from django.template import RequestContext
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
+
 
 def home(request):
     info = BlogInfo.objects.get(pk = 1)
-    return render_to_response('index.html', {'info': info})
+    return render(request, 'index.html', {'info': info})
 
 def about(request):
-    return render_to_response('about.html')
+    return render(request, 'about.html')
 
 def contact(request):
-    return render_to_response('contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('contact.html')
+
+    return render(request, 'contact.html')
 
 def blog(request, pk):
     blog = Article.objects.get(pk = int(pk))
-    return render_to_response('single.html', {'blog': blog})
+    return render(request, 'single.html', {'blog': blog})
